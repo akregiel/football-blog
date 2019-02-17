bcd213e798d09f7da235e7030846adef8dd06c27
akregiel <a-kre@wp.pl>
2019-02-17 18:31:47 +0100

    Modified templates to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Wpis
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts=Wpis.objects.filter(data_publikacji__lte=timezone.now()).order_by('data_publikacji')
+    return render(request, 'blog/post_list.html', {'posts': posts})
